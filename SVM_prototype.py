4496bf68729dcdd62b6da91947b9258b3eeed426
Khanh Nguyen <nh.minhkhanhLA@gmail.com>
2019-12-07 15:04:16 -0800

    Update ipython notebook

--- SVM_prototype.py
+++ SVM_prototype.py
@@ -8,29 +8,36 @@ Created on Mon Oct 21 16:23:09 2019
 #================Importing section=========================
 import pandas as pd
 import numpy as np
-import tensorflow as tf
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import json
 import os
 import cv2
 import pickle
+import csv
 
 from PIL import Image
 from skimage.feature import hog
 from skimage.color import rgb2grey
+from skimage.filters import prewitt_h,prewitt_v, sobel
 
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+# Library for scikit-learn
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-from sklearn.svm import SVC
-from sklearn.metrics import roc_curve, auc
 
+# Library for LIBSVM
+from libsvm.svmutil import *
 from itertools import combinations
+from skimage.data import camera
+
+# Library fror tensorflow and keras
+import tensorflow as tf
+from tensorflow import keras
+#from keras import Activation, BatchNormalization, Conv2D, Dense, Dropout, Flatten, MaxPooling2D, Sequential, optimizers
+import h5py
 #================End Importing section=====================
 
-# check if tkinter working
+#check if tkinter working
 if os.environ.get('DISPLAY','') == '':
     print('no display found. Using non-interactive Agg backend')
     mpl.use('Agg')
@@ -40,13 +47,14 @@ parent_dir = "/mnt/c/Users/nhmin/Downloads/food-101/"
 bin_n = 16 # Number of bin
 project_dir = os.getcwd()
 model_dir = "/mnt/c/Users/nhmin/Downloads/"
-class_label = ["pad_thai", "pho", "ramen", "spaghetti_bolognese", "spaghetti_carbonara"]
-# svm_params = dict(kernel_type= cv2.SVM_LINEAR, svm_type=cv2.SVM_C_SVC, C=2.67, gamma=5.383)
+class_label = {"pad_thai" : 0, "pho" : 1, "ramen" : 2, "spaghetti_bolognese" : 3, "spaghetti_carbonara" : 4}
 
+
+# Return np img size 128x128
 def get_image(path):
-    # image size = 512x512
+    # image resize to 384x384
     img = Image.open(path + ".jpg")
-    resized_image = img.resize((384,384), Image.ANTIALIAS)
+    resized_image = img.resize((128,128), Image.ANTIALIAS)
     return np.array(resized_image)
 
 
@@ -55,135 +63,128 @@ def load_json(path):
     # Load in json file to create dictionary: key = class label; value = file path
     with open(path, 'r') as file:
         data = json.load(file)
+    # Only get information from needed class
     for label in class_label:
         final_data.update({label : data.get(label)})
     return final_data
 
-def load_HOG_data(data):
-    feature_lists = []
-    for image in data:
-            file_name = image.split('/')[1]
-            file_image = get_image(parent_dir + "images/" + image)
-            # given 32x32 cell
-            image_feature, image_hog = hog(file_image, orientations=8, pixels_per_cell=(16, 16),
-                    cells_per_block=(8, 8), block_norm = 'L2-Hys', visualize=True, multichannel=True)
-            feature_lists.append(image_feature)
-    return np.array(feature_lists)
+def HOG_image(image):
+    np_image = get_image(parent_dir + "images/" + image)
+    # given 32x32 cell
+    image_feature, image_hog = hog(np_image, orientations=8, pixels_per_cell=(8, 8),
+        cells_per_block=(8, 8), block_norm = 'L2-Hys', visualize=True, multichannel=True)
+    return np.array(image_feature)
 
-def label_processing(data, label_dictionary):
-    final_label = []
-    for i in label_dictionary.keys():
-        label_value = label_dictionary.get(i)
-        for image in data.get(i):
-            final_label.append(label_value)
-    return np.array(final_label)
-
-def feature_format(data):
-    feature_matrix = load_HOG_data(data)
+def pca_transform(image):
     ss = StandardScaler()
-    food_stand = ss.fit_transform(feature_matrix)
-    pca = PCA(n_components = feature_matrix.shape[0])
-    food_pca = ss.fit_transform(food_stand)
-    return food_pca
+    image_ss = ss.fit_transform(image)
+    # Keep 90% of variance
+    pca = PCA(0.85)
+    image_pca = pca.fit_transform(image_ss)
+    return image_pca
 
-def pre_process_data(data_json):
-    pca_data = dict()
-    labels = list(data_json.keys())
-    for label in labels:
-        pca_data.update({label : feature_format(data_json.get(label))})
-    return pca_data
+def get_key(val):
+    for key, value in class_label.items():
+        if(val == value): return key
+    return None
 
-def train_dataset(train_json):
+def load_train_data():
+    with h5py.File(model_dir + '227-imgsz-32-bsz-0.01-lr-30-ep.ckpt.02-1.64.hdf5', 'r') as f:
+        print(f.keys())
+        data = f['model_weights']
+        for name in data:
+            print(name)
+        print(data['max_pooling2d_18'])
+        # print(data.shape)
+        # print(data.dtype)
+        # print(f['model_weights'][0])
+        # print("===============")
+        # print(f['optimizer_weights'])
+
+def training():
+    # load json file
+    train_json = load_json(parent_dir + "/meta/train.json")
+
     # Getting all the label and combination of all label
     labels = list(train_json.keys())
     class_combinations = list(combinations(labels, 2))
 
-    #Get all pca dictinary data for both train and test
-    train_pca_dict = pre_process_data(train_json)
-    for combination in class_combinations:
-        #SVM classification
-        svm = SVC(gamma='auto', kernel='linear', probability=True)
-        combine_train_data = np.vstack((train_pca_dict.get(combination[0]), train_pca_dict.get(combination[1])))
-        label_dict = {combination[0] : 1, combination[1] : -1}
-        label_lists_train = label_processing(train_json, label_dict)
+    # Create all train data after HOG
+    train_image = []
+    train_data = []
+    train_label = []
+    for label in train_json.keys():
+        for img in train_json.get(label):
+            train_image.append(img.split('/')[1])
+            train_label.append(class_label.get(label))
+            train_data.append(HOG_image(img))
+    # Applying PCA 
+    train_pca = pca_transform(train_data)
+    train_model = svm_train(train_label, train_data, '-s 0 -t 0 -c 2.3 -b 1')
+    svm_save_model('all_food_classification.model', train_model)
+    
 
-        x_train = pd.DataFrame(combine_train_data)
-        y_train = pd.Series(label_lists_train)
+def testing():
+    # load json file
+    test_json = load_json(parent_dir + "/meta/test.json")
+    model = svm_load_model('all_food_classification.model')
+    # Create all train data after HOG
+    test_image = []
+    test_data = []
+    test_label = []
+    for label in test_json.keys():
+        for img in test_json.get(label):
+            test_image.append(img.split('/')[1])
+            test_label.append(class_label.get(label))
+            test_data.append(HOG_image(img))
+    
+    p_label, p_acc, p_val = svm_predict(test_label, test_data, model, '-b 1')
+    # acc, mse, scc = evaluations(test_label, p_label)
+    #print("Test acc: ", acc)
+    for i in range(0, len(test_label)):
+        if(test_label[i] != p_label[i]):
+            print(test_image[i])
+            print("True label: ", get_key(test_label[i]), " Predict label: ", get_key(p_label[i]))
 
-        svm.fit(x_train, y_train)
-        filename = combination[0] + "_" + combination[1] + "_model.sav"
-        model_filename = model_dir + "/models_save/" + filename
-        pickle.dump(svm, open(model_filename, 'wb'))
-def test_dataset(test_json):
-    # Getting all the label and combination of all label
-    labels = list(test_json.keys())
-    class_combinations = list(combinations(labels, 2))
-    #Get all pca dictinary data for both train and test
-    test_pca_dict = pre_process_data(test_json)
+def show_pca(image_path):
+    image_grey = Image.open(image_path + ".jpg").convert('L')
+    image_grey.show()
+    image_np = np.array(image_grey.resize((128,128), Image.ANTIALIAS))
+    print(image_np.shape)
+    image_pca = pca_transform(image_np)
+    image_rgb = Image.fromarray(image_pca, 'L')
+    image_rgb.show()
 
-    for combination in class_combinations:
-        # load in SVM model
-        filename = combination[0] + "_" + combination[1] + "_model.sav"
-        loaded_model = pickle.load(open(model_dir + filename, 'rb'))
-        combine_test_data = np.vstack((test_pca_dict.get(combination[0]), test_pca_dict.get(combination[1])))
-        label_dict = {combination[0] : 1, combination[1] : -1}
-        label_lists_test = label_processing(test_json, label_dict)
 
-        x_test = pd.DataFrame(combine_test_data)
-        y_test = pd.Series(label_lists_test)
 
-        accuracy = loaded_model.score(x_test, y_test)
-        y_pred = loaded_model.predict(x_text)
-        print(combination)
-        print("Test accuracy: " + accuracy)
-        print("Test set prediction: " + y_pred)
 
+def single_test(image_path):
+    #image_np = get_image(image_path)
+    image_hog = HOG_image(image_path)
+    print(image_hog)
+    model = svm_load_model('all_food_classification.model')
+    p_label, p_acc, p_val = svm_predict([], [image_hog], model, '-b 1')
+    print(p_label)
+    print(get_key(p_label[0]))
+    print(p_val)    
+# DON'T NEED FOR NOW
+# def mean_pixel_image(image):
+#     np_image = get_image(parent_dir + "images/" + image)
+#     img_shape = np_image.shape
+#     temp_img = np.zeros((img_shape[0], img_shape[1]))
+#     for i in range(0, img_shape[0]):
+#         for j in range(0, img_shape[1]):
+#             temp_img[i][j] = (int(np_image[i,j,0]) + int(np_image[i,j,1]) + int(np_image[i,j,2]))/3
+#     return np.array(np.reshape(temp_img, (img_shape[0],img_shape[1]))).ravel()
 
 def main():
-    # Compute both train and test data
-    # load json file
-    train_json = load_json(parent_dir + "/meta/train.json")
-    test_json = load_json(parent_dir + "/meta/test.json")
-
-    train_dataset(test_json)
-    test_dataset(test_json)
-
-    # Getting all the label and combination of all label
-    # labels = list(train_json.keys())
-    # class_combinations = list(combinations(labels, 2))
-    # #Get all pca dictinary data for both train and test
-    # train_pca_dict = pre_process_data(train_json)
-    # test_pca_dict = pre_process_data(test_json)
-
-    # for combination in class_combinations:
-    #     print(combination)
-    #     #SVM classification
-    #     svm = SVC(C=2.67,gamma='auto', kernel='linear', probability=True)
-    #     combine_train_data = np.vstack((train_pca_dict.get(combination[0]), train_pca_dict.get(combination[1])))
-    #     print(combine_train_data)
-    #     print(combine_train_data.shape)
-    #     combine_test_data = np.vstack((test_pca_dict.get(combination[0]), test_pca_dict.get(combination[1])))
-    #     label_dict = {combination[0] : 1, combination[1] : -1}
-    #     label_lists_train = label_processing(train_json, label_dict)
-    #     label_lists_test = label_processing(test_json, label_dict)
-
-    #     x_train = pd.DataFrame(combine_train_data)
-    #     y_train = pd.Series(label_lists_train)
-
-    #     x_test = pd.DataFrame(combine_test_data)
-    #     y_test = pd.Series(label_lists_test)
-
-    #     svm.fit(x_train, y_train)
-    #     filename = combination[0] + "_" + combination[1] + "_model.sav"
-    #     model_filename = model_dir + "/models_save/" + filename
-    #     pickle.dump(svm, open(model_filename, 'wb'))
-
-    #     y_pred = svm.predict(x_test)
-    #     #calculate accuracy
-    #     accuracy = accuracy_score(y_test, y_pred)
-    #     print(combination)
-    #     print("Model accuracy: ", accuracy)
-
+    load_train_data()
+    #training()
+    print("model training is finished")
+    #testing()
+    #print("Single image test.")
+    #single_test("pho/34077")
+    #show_pca(parent_dir + "images/pho/34077")
 
 
 if __name__ == '__main__':
